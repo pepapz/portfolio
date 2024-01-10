e32e28ff3ab955634b44eef7559aa99c1cd666f4
oracleyes99 <81101732+pepapz@users.noreply.github.com>
2024-01-10 18:15:19 +0900

    0110_2

---
+++ index_ikea/js/script.js
@@ -0,0 +1,9 @@
+//nav
+function openNav() {
+    document.getElementById("subNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("subNav").style.width = "0%";
+  }
+  
